40228ffdefc770f51f4b97eff64784e00b33f241
Leon Xu <dcwinfield362@gmail.com>
2022-05-05 18:53:00 -0700

    fix: closing tag error

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  return <section className="home">Home</sec>;
+  return <section className="home">Home</section>;
 }
 
 export default Home;
