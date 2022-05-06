13d468557870746e4365d2bfd1803d07deb1b2a5
Leon Xu <dcwinfield362@gmail.com>
2022-05-05 18:24:15 -0700

    doc: add header__logo, header__search and header__nav class to Header component

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,7 +2,33 @@ import React from "react";
 import "./Header.css";
 
 function Header() {
-  return <header className="header"></header>;
+  return (
+    <header className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+
+      <section className="header__search">
+        <input className="header__searchInput" type="text" />
+      </section>
+
+      <nav className="header__nav">
+        <section className="header__option">
+          <span className="header__optionLineOne">Hello</span>
+          <span className="header__optionLineTwo">Sign In</span>
+        </section>
+        <section className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </section>
+        <section className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </section>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
