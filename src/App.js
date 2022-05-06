83b2b702147acb9b1ecd174f416c7c34f9d1ea5a
Leon Xu <dcwinfield362@gmail.com>
2022-05-05 20:50:57 -0700

    doc: add React Router to the entire application

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // Import file
 import Header from "./Header/Header";
@@ -8,10 +9,16 @@ import "./App.css";
 
 function App() {
   return (
-    <section className="app">
-      <Header />
-      <Home />
-    </section>
+    <Router>
+      <section className="app">
+        <Switch>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </section>
+    </Router>
   );
 }
 
