065660e66219dbc5eb8737e435544f2352eed3b0
Leon Xu <dcwinfield362@gmail.com>
2022-05-05 18:56:29 -0700

    doc: add home__image and home__row that will contains a product

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  return <section className="home">Home</section>;
+  return (
+    <section className="home">
+      <article className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt="amazon home image"
+        />
+      </article>
+
+      <article className="home__row"></article>
+      <article className="home__row"></article>
+      <article className="home__row"></article>
+    </section>
+  );
 }
 
 export default Home;
