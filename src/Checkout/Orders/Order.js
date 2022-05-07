d4bb3b81faa0add7f65f97ab8adb982112e4a139
Leon Xu <dcwinfield362@gmail.com>
2022-05-07 15:25:01 -0700

    file: initialize Orders component and Order component along with its stylesheet

---
+++ src/Checkout/Orders/Order.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Order.css";
+
+function Order() {
+  return <section>Order</section>;
+}
+
+export default Order;
---
+++ src/Checkout/Orders/Orders.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Orders.css";
+
+function Orders() {
+  return <section className="orders">Orders</section>;
+}
+
+export default Orders;
