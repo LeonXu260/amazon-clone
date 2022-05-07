94ecbadbf3e8340a51ce3f63c70d57b165368e7d
Leon Xu <dcwinfield362@gmail.com>
2022-05-07 13:49:40 -0700

    doc: update payment component

--- src/Checkout/Payment/Payment.js
+++ src/Checkout/Payment/Payment.js
@@ -1,8 +1,49 @@
 import React from "react";
+import { useStateValue } from "../../redux/StateProvider";
+
+import CheckoutProduct from "../CheckoutProduct";
 import "./Payment.css";
 
 function Payment() {
-  return <section className="payment">Payment</section>;
+  const [{ basket, user }, dispatch] = useStateValue();
+  return (
+    <section className="payment">
+      <article className="payment__container">
+        <article className="payment__section">
+          <article className="payment__title">
+            <h3>Delivery Address</h3>
+          </article>
+          <article className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 React Lane</p>
+            <p>Los Angeles, CA</p>
+          </article>
+        </article>
+        <article className="payment__section">
+          <article className="payment__title">
+            <h3>Review items and delivery</h3>
+          </article>
+          <article className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </article>
+        </article>
+        <article className="payment__section">
+          <article className="payment__title">
+            <h3>Payment Method</h3>
+          </article>
+          <article className="payment__details"></article>
+        </article>
+      </article>
+    </section>
+  );
 }
 
 export default Payment;
