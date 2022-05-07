7b96e90ea8edc3f42a2ce0c6dda1a0b2ad2cc36e
Leon Xu <dcwinfield362@gmail.com>
2022-05-07 14:02:02 -0700

    doc: add CardElement, useElements, useStripe from react-stripe-js and handleChange and handleSubmit method

--- src/Checkout/Payment/Payment.js
+++ src/Checkout/Payment/Payment.js
@@ -3,9 +3,16 @@ import { useStateValue } from "../../redux/StateProvider";
 import { Link } from "react-router-dom";
 import CheckoutProduct from "../CheckoutProduct";
 import "./Payment.css";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = (e) => {};
+  const handleChange = (e) => {};
+
   return (
     <section className="payment">
       <article className="payment__container">
@@ -42,7 +49,11 @@ function Payment() {
           <article className="payment__title">
             <h3>Payment Method</h3>
           </article>
-          <article className="payment__details"></article>
+          <article className="payment__details">
+            <form onClick={handleSubmit}>
+              <CardElement onChange={handleChange} />
+            </form>
+          </article>
         </article>
       </article>
     </section>
