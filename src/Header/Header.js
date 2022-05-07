2e512535a0b281b73eda68a24eec84c7cc949122
Leon Xu <dcwinfield362@gmail.com>
2022-05-07 13:14:54 -0700

    doc: add conditional rendering to Hello text

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -32,7 +32,9 @@ function Header() {
       <nav className="header__nav">
         <Link to={!user && "/login"}>
           <section onClick={authHandle} className="header__option">
-            <span className="header__optionLineOne">Hello</span>
+            <span className="header__optionLineOne">
+              Hello, {!user ? "Guest" : user.email}
+            </span>
             <span className="header__optionLineTwo">
               {user ? "Sign Out" : "Sign In"}
             </span>
